3e619e95de49b2324050d3f66d32ce228af3d733
Shafkatahmad <shafkataahmad@gmail.com>
2026-05-07 01:15:39 +0600

    class

--- solutions.ts
+++ solutions.ts
@@ -71,3 +71,31 @@ const myBook = {
 };
 toggleReadStatus(myBook);
 // console.log(toggleReadStatus(myBook));
+
+// Class
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+class Student extends Person {
+  grade: string;
+
+  constructor(name: string, age: number, grade: string) {
+    super(name, age);
+    this.grade = grade;
+  }
+
+  getDetails() {
+    return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
+  }
+}
+
+const student = new Student("Alice", 20, "A");
+student.getDetails();
+// console.log(student.getDetails());
